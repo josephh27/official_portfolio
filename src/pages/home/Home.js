44474f2664a09fb8d3a3a7b67d1a63a6e936f08b
Buhain, Joseph Jason S <josephjasonsbuhain@iskolarngbayan.pup.edu.ph>
2024-03-09 23:59:06 +0800

    added some elements in the hero section.

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,30 +1,33 @@
 import React from 'react';
-import './Home.scss'
+import './Home.scss';
+import myImage from '../../assets/images/owner.png';
 
 const Home = () => {
   return (
     <div className="home-wrapper">
-      <div className="sections-container">
-          <div className="section">
-            
-          </div>
-          <div className="section">
-            
-          </div>
-          <div className="section">
-            
-          </div>
-          <div className="section">
-            
-          </div>
-          <div className="section">
-            
-          </div>
-          <div className="section">
-            
-          </div>
-          
+      <div className="inner-wrapper">
+        <div className="sections-container">
+            <div className="section hero">
+              <img src={myImage} alt="Joseph Jason Buhain" />
+            </div>
+            <div className="section">
+              
+            </div>
+            <div className="section">
+              
+            </div>
+            <div className="section">
+              
+            </div>
+            <div className="section">
+              
+            </div>
+            <div className="section">
+              
+            </div>            
+        </div>
       </div>
+
     </div>
   )
 }
